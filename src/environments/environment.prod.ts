cdec252c6381d212ca8a16bf8ade859c76e76d0d
janak <janakrajbhatta@gmail.com>
2018-02-15 08:49:08 +0545

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,  
-  baseUrl: 'http://localhost:3000/api/',
-  staticUrl: 'http://localhost:3000/'
+  baseUrl: 'http://meromoviesapi.herokuapp.com/api/',
+  staticUrl: 'http://meromoviesapi.herokuapp.com/'
 };
